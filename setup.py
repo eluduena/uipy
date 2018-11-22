4c540cb4bc9ef8df73ff695228bc9ef0f6796fc8
electronica <electronica@bupu>
2018-11-22 15:47:33 -0300

    Add ipython3 package and requerient python3

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
    description='A user interface for ipython3',
    author='Emilio L.',
    author_email='eludena@gmail.com',
+   python_requires='>3',
+   install_requires=[
+   'ipython'
+   ],
    #packages=['uipy3'],
    #package_dir={'uipy3': 'src/uipy3'},
    # package_data={'mypkg': ['data/*.dat']},
