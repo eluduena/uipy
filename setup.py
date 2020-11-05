2118897b3ae8624643f7bcf16381669237584c91
eluduena <eludena@gmail.com>
2020-11-04 22:39:09 -0300

    change uipy3 to uipy

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 setup(
-   name='uipy3',
+   name='uipy',
    version='1.0',
-   description='A user interface for ipython3',
+   description='A user interface for ipython',
    author='Emilio L.',
    author_email='eludena@gmail.com',
    python_requires='>3',
    install_requires=[
    'ipython'
    ],
-   #packages=['uipy3'],
-   #package_dir={'uipy3': 'src/uipy3'},
+   #packages=['uipy'],
+   #package_dir={'uipy': 'src/uipy'},
    # package_data={'mypkg': ['data/*.dat']},
    scripts=[
-            'scripts/uipy3',
+            'scripts/uipy',
            ]
 )
